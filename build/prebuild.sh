e520ada9345a01bc437e021733af463af59b0c74
tatsuma makino <devel.mckee.1531@gmail.com>
2022-05-13 22:10:42 +0900

    update

--- build/prebuild.sh
+++ build/prebuild.sh
@@ -4,22 +4,23 @@ echo "Set environment variables"
 echo "current directory is $(pwd)"
 set -e
 if [ -z "$PROJECT_ROOT_DIR" ]; then
+  export PROJECT_ROOT_DIR
   PROJECT_ROOT_DIR="$(pwd)"
 fi
 if [ -z "$BUILD_ENV" ]; then
-  BUILD_ENV=stg
+  export BUILD_ENV=stg
 fi
 
 echo "Setup hugo config"
 set -e
 if [ -z "$HUGO_ENV" ]; then
-  HUGO_ENV="${BUILD_ENV}"
+  export HUGO_ENV="${BUILD_ENV}"
 fi
 if [ -z "$HUGO_THEME" ]; then
-  HUGO_THEME="techblog"
+  export HUGO_THEME="techblog"
 fi
 if [ -z "$HUGO_BASEURL" ]; then
-  HUGO_BASEURL="https://${AWS_BRANCH}.${AWS_APP_ID}.amplifyapp.com/"
+  export HUGO_BASEURL="https://${AWS_BRANCH}.${AWS_APP_ID}.amplifyapp.com/"
 fi
 
 echo "Install hugo theme assets' dependencies"
