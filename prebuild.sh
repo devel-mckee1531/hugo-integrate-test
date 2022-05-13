a4f394a4093f0ecaf3261b072cb8b1fcf55d99e0
tatsuma makino <devel.mckee.1531@gmail.com>
2022-05-13 19:30:31 +0900

    test

--- prebuild.sh
+++ prebuild.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-set -eux
+set -ux
 
-echo "HOME is $HOME"
-echo "Setup hugo config"
+printenv
 
 echo "current directory is $(pwd)"
 if [ -z "$HUGO_BASE_URL" ]; then
   HUGO_BASE_URL="https://${AWS_BRANCH}.${AWS_APP_ID}.amplifyapp.com/"
 fi
+
+set -eux
+
+echo "Setup hugo config"
 
 echo "Install hugo theme assets' dependencies"
 
